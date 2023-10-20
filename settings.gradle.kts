8c016fec74f9974af6ed1383c0406e9ee60dd569
oosinloye <oosinloye@wisc.edu>
2023-10-20 17:59:34 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lab05_milestone1"
+include(":app")
+ 
